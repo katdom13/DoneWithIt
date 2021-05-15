42028f62491bc73577cf891dfcdb7096b99d700d
katdom13 <katdom13@gmail.com>
2021-05-16 01:36:13 +0800

    Added Views, Text, Image, Touchables, Button, Alerts, Platform specific code, Events

--- App.js
+++ App.js
@@ -1,24 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  Button,
+  ScrollView,
+  Alert
+} from 'react-native';
 
 export default function App() {
 
-  // Logging
-  // Only use during developments!
+  // Does not work on android
+  // const handleOwlImagePress = () => Alert.prompt("Owl Image Tapped",
+  //   "What is the name of this owl?",
+  //   text => console.log(text),
+  // )
 
-  let x = 1
-  console.log('App executed')
+  const handleOwlImagePress = () => Alert.alert("Owl Image Tapped",
+    "This owl's name is Numnums",
+    [
+      {
+       text: 'Cancel'
+      },
+      {
+        text: 'Ok',
+        onPress: () => console.log('Met Numnums'),
+      }
+    ]
+  )
 
-  // Debugging in browser
-  // let x
-  // x.toString()
 
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    // SafeAreaView does not work on android
+    <SafeAreaView style={styles.container}>
+
+      {/* For scrolling */}
+      <ScrollView contentContainerStyle={styles.scrollView}>
+
+      {/* Buttons and Alerts */}
+      <Button
+        title="Click Me"
+        style={styles.button}
+        color="pink"
+        onPress={() => Alert.alert("Alert", "Button Tapped")}
+      />
+
+      <Text numberOfLines={2} onPress={() => console.log('Text pressed')}>
+        Hello React Native - This is a very very loooooooong text! This should demonstrate some args for Text component.
+      </Text>
+
+      {/* Since the image component does not have an 'onPress' event,
+        We can wrap it in a 'Touchable' component
+      */}
+      <TouchableOpacity onPress={handleOwlImagePress} activeOpacity={0.8}>
+        {/* Require includes the picture in the bundler. This makes the app larger */}
+        <Image source={require('./assets/channels4_banner.jpg')} />
+      </TouchableOpacity>
+
+      {/* It is more appropriate to download from the web */}
+      {/* But it is required to specify the dimensions */}
+      <Image
+        source={{width: 400, height: 400, uri: 'https://reactjs.org/logo-og.png'}}
+        // Blurs the picture
+        blurRadius={1}
+        // Gives a fade effect (works only on android)
+        fadeDuration={1000}
+      />
       <StatusBar style="auto" />
-    </View>
+
+      </ScrollView>
+
+    </SafeAreaView>
   );
 }
 
@@ -26,7 +83,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // Manually fix margin to get out of statusbar
+    // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  scrollView: {
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 100,
+  }
 });
