98c4f594b255114a9804d99ee7513ffc74d99c00
katdom13 <katdom13@gmail.com>
2021-05-17 11:00:56 +0800

    Reviewed flex

--- App.js
+++ App.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import {
   View,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-  StyleSheet,
-  Dimensions,
 } from 'react-native';
 import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks'
 
 export default function App() {
-
-  console.log(useDimensions())
-
-  // Use this hook to detect screen orientation
-  const {landscape} = useDeviceOrientation()
-  
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={{
+      backgroundColor: '#fff',
+      flex: 1,
+    }}>
       <View style={{
         backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: landscape ? '100%' : '30%',
-      }}></View>
-    </SafeAreaView>
+        flex: 2,
+      }}
+      />
+      <View style={{
+        backgroundColor: 'gold',
+        flex: 1,
+      }}
+      />
+      <View style={{
+        backgroundColor: 'tomato',
+        flex: 1,
+      }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-  }
-});
